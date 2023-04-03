218d557238fa4d7e98837ba4295bf3439dfda16b
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2023-04-03 21:25:41 +0200

    replace youtube_dl with yt-dlp
    right now youtube_dl doesn't work with youtube, ironic

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from discord.ext import commands
 from discord.utils import get
 from discord import FFmpegPCMAudio
 from discord import TextChannel
-from youtube_dl import YoutubeDL
 from discord import Intents
+from yt_dlp import YoutubeDL
 
 load_dotenv()
 intents = Intents().default()
