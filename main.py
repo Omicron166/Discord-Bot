d0ed4bf1941539c87b3d5459e095371fd2d534d4
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2023-07-15 17:25:42 +0200

    fix some shit and re-enable join

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ else:
 with open(path, 'r') as f:
     config = json.load(f)
 
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
 intents = Intents().default()
 intents.message_content = True
 
@@ -24,33 +27,29 @@ if args[0].endswith('.exe'):
 
 client = commands.Bot(command_prefix=config["prefix"], intents=intents)  # prefix our commands with '.'
 
-#players = {}
-
 @client.event  # check if bot is ready
 async def on_ready():
     print('Bot online as', client.user.name)
 
 
 # command for bot to join the channel of the user, if the bot has already joined and is in a different channel, it will move to the channel the user is in
-#@client.command()
-#async def join(ctx):
-#    try:
-#        channel = ctx.message.author.voice.channel
-#    except AttributeError:
-#        await ctx.send('Please join a voice channel')
-#        return
-#    voice = get(client.voice_clients, guild=ctx.guild)
-#    if voice and voice.is_connected():
-#        await voice.move_to(channel)
-#    else:
-#        voice = await channel.connect()
+@client.command()
+async def join(ctx):
+    try:
+        channel = ctx.message.author.voice.channel
+    except AttributeError:
+        await ctx.send('Please join a voice channel')
+        return
+    voice = get(client.voice_clients, guild=ctx.guild)
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
 
 
 # command to play sound from a youtube URL
 @client.command()
 async def play(ctx, url):
-    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
-    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
     voice = get(client.voice_clients, guild=ctx.guild)
 
     if not voice:
@@ -69,11 +68,10 @@ async def play(ctx, url):
         await ctx.send('Trying to play the song')
         with YoutubeDL(YDL_OPTIONS) as ydl:
             info = ydl.extract_info(url, download=False)
-        URL = info['url']
-        voice.play(FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        voice.play(FFmpegPCMAudio(info['url'], **FFMPEG_OPTIONS))
         await ctx.send('Bot is playing')
 
-# check if the bot is already playing
+    # check if the bot is already playing
     else:
         await ctx.send("Bot is already playing")
         return
